50c954172814123f70bc6673143140f9d5270206
h-hibaaah <hibba5825@gmail.com>
2026-08-06 18:54:27 +0500

    Decryption Code Ran Successfully

--- src/decryption.cpp
+++ src/decryption.cpp
@@ -1,8 +1,43 @@
 #include "decryption.h"
+#include <fstream>
 #include <iostream>
 using namespace std;
 #include <string>
 void Decryption::decrypt()
 {
-    cout<<"Decrypt Function Called"<<endl;
+    string line;
+    string decoded;
+    char ch;
+    ifstream file("data/encrypted.txt");
+    if (!file.is_open()) {
+        cout << "Error opening file" << endl;
+        return;
+    }
+    while (getline(file, line)) {
+        
+        for (int i = 0; i < line.length(); i++)
+        {
+            ch = line[i];
+            if (ch >= 'A' && ch <= 'Z')
+            {
+                ch = ((ch - 'A' - 3 + 26) % 26) + 'A';
+              decoded += ch;
+            }
+            else if (ch >= 'a' && ch <= 'z')
+            {
+                ch = ((ch - 'a' - 3 + 26) % 26) + 'a';
+                decoded += ch;
+            }
+            else
+            {
+                decoded += ch;
+            }
+        }
+        cout << endl;
+    }
+    ofstream outputFile("data/decrypted.txt");
+    outputFile << decoded;
+    outputFile.close();
+    cout << "Decryption completed. Decrypted text saved to data/decrypted.txt" << endl;
+
 }
