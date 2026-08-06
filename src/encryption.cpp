36d65255f4efd14e275097eee56083b13fa16bc9
h-hibaaah <hibba5825@gmail.com>
2026-08-06 14:59:01 +0500

    Encryption Code is Added

--- src/encryption.cpp
+++ src/encryption.cpp
@@ -1,8 +1,37 @@
 #include "encryption.h"
+#include <fstream>
 #include <iostream>
 using namespace std;
 #include <string>
 void Encryption::encrypt()
 {
-    cout<<"Encrypt Function Called"<<endl;
+    char ch;
+    string coded;
+    ifstream file("data/input.txt");
+    string line;
+    if (!file.is_open()) {
+        cout << "Error opening file" << endl;
+        return;
+    }
+while (getline(file, line))
+{
+     for (int i = 0; i < line.length(); i++)
+     {
+        ch = line[i];
+        if (ch >= 'A' && ch <= 'Z')
+        {
+            ch = ((ch - 'A' + 3))% 26 + 'A';
+            coded += ch;
+        }
+         else if (ch >= 'a' && ch <= 'z')
+        {
+            ch = ((ch - 'a' + 3))% 26 + 'a';
+            coded += ch;
+        }
+        else
+        {
+            coded += ch;
+        }
+     }
+}
 }
